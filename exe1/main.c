4b903a8f169386f0bf51f67416ff105761170525
Dovlask <laskdov@gmail.com>
2026-04-14 10:59:50 -0300

    Exe1 e ex2

--- exe1/main.c
+++ exe1/main.c
@@ -14,12 +14,25 @@ void led_1_task(void *p) {
   gpio_init(LED_PIN_R);
   gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
-  int delay = 250;
+  int t = 250;
   while (true) {
     gpio_put(LED_PIN_R, 1);
-    vTaskDelay(pdMS_TO_TICKS(delay));
+    vTaskDelay(pdMS_TO_TICKS(t));
     gpio_put(LED_PIN_R, 0);
-    vTaskDelay(pdMS_TO_TICKS(delay));
+    vTaskDelay(pdMS_TO_TICKS(t));
+  }
+}
+
+void led_2_task(void *p) {
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  int u = 250;
+  while (true) {
+    gpio_put(LED_PIN_G, 1);
+    vTaskDelay(pdMS_TO_TICKS(u));
+    gpio_put(LED_PIN_G, 0);
+    vTaskDelay(pdMS_TO_TICKS(u));
   }
 }
 
@@ -27,6 +40,7 @@ int main() {
   stdio_init_all();
   printf("Start RTOS \n");
   xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
+  xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
   vTaskStartScheduler();
 
   while (true)
